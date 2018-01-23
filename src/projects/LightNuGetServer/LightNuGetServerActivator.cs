35f25f9130ecfb4e440e2e78e640873c96b59fec
Carlos A. Osoria <carlos.osoria@gmail.com>
2018-01-23 12:01:10 -0600

    Added multi repository support

--- src/projects/LightNuGetServer/LightNuGetServerActivator.cs
+++ src/projects/LightNuGetServer/LightNuGetServerActivator.cs
@@ -24,18 +24,22 @@ namespace LightNuGetServer
 
             var logger = new SerilogProxyLogger(Log.Logger);
 
-            var feeds = settings.Feeds
-                .Select(fs => new LightNuGetFeed(fs, NuGetV2WebApiEnabler.CreatePackageRepository(Path.Combine(settings.PackagesDirPath, fs.Name), fs, logger)))
-                .ToArray();
-
-            cfg?.Invoke(feeds);
-
-            foreach (var feed in feeds)
+            foreach (var repo in settings.Repositories)
             {
-                config.UseNuGetV2WebApiFeed(
-                    routeName: feed.Name,
-                    routeUrlRoot: feed.Slug,
-                    oDatacontrollerName: "LightNuGetFeed");
+                var feeds = repo.Feeds
+                    .Select(fs => new LightNuGetFeed(fs, NuGetV2WebApiEnabler.CreatePackageRepository(Path.Combine(repo.PackagesDirPath, fs.Name), fs, logger)))
+                    .ToArray();
+
+                cfg?.Invoke(feeds);
+
+                foreach (var feed in feeds)
+                {
+                    config.UseNuGetV2WebApiFeed(
+                        routeName: feed.Name,
+                        routeUrlRoot: feed.Slug,
+                        oDatacontrollerName: "LightNuGetFeed");
+                }
+
             }
 
             return app;
--- src/projects/LightNuGetServer/LightNuGetServerSettings.cs
+++ src/projects/LightNuGetServer/LightNuGetServerSettings.cs
@@ -2,6 +2,11 @@
 {
     public class LightNuGetServerSettings
     {
+        public LightNuGetServerRepositorySettings[] Repositories { get; set; } = new LightNuGetServerRepositorySettings[0];
+    }
+
+    public class LightNuGetServerRepositorySettings
+    {
         public string PackagesDirPath { get; set; } = "Packages";
         public LightNuGetFeedSettings[] Feeds { get; set; } = new LightNuGetFeedSettings[0];
     }
